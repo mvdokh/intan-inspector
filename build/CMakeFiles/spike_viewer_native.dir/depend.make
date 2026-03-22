bb060537a3234acbbec0003e3d5e65c6982e3073
Martin Dokholyan <martin@dokholyan.org>
2026-03-22 12:03:45 -0400

    Delete build directory

--- build/CMakeFiles/spike_viewer_native.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for spike_viewer_native.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/spike_viewer_native_autogen_timestamp_deps.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for spike_viewer_native_autogen_timestamp_deps.
